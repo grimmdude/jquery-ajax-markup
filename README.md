b568664e9e1b2aca0d6ee106cf1e7fb58724ca8f
Garrett Grimm <ggrimm@prudentpublishing.com>
2015-03-21 11:02:59 -0700

    1.2.0

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # jquery-ajax-markup
-Simple jQuery library to fill a container with ajax results using HTML markup only.
+Simple jQuery plugin to fill a container with ajax results using HTML markup only.
 
 ##Usage
 
 With this markup the contents of the div will be replaced by the response of the url.
-```html
+```html\
 <div data-ajax-url="http://example.com/api/product"></div>
 ```
 
@@ -12,3 +12,5 @@ In this example `json` is specified as the data type and the `name` text will be
 ```html
 <div data-ajax-url="http://example.com/api/product" data-ajax-type="json">name</div>
 ```
+
+Note that urls are only requested once even if multiple elements use the same one.
--- jquery-ajax-markup.js
+++ jquery-ajax-markup.js
@@ -1,24 +1,45 @@
 (function ( $ ) {
     $(function() {
-    	$('*[data-ajax-url]').each(function(index, element) {
-    		var innerText = $(element).text();
-    		var elementData = $(element).data();
-    		var dataType = elementData.ajaxType || false;
-            
-            $.ajax(elementData.ajaxUrl, {
-    			dataType: dataType,
-    			success: function(response) {
-                    if (innerText != '') {
-                        $(element).text(response[innerText]);
+        var urls = {}; // url : dataType 
+        var $ajaxMarkupElements = $('*[data-ajax-url]');
+
+        // First get a list of all the urls to be called.
+        $ajaxMarkupElements.each(function(index, element) {
+            var elementData = $(element).data();
+            var dataType = elementData.ajaxType || false;
+
+            if (!urls.hasOwnProperty(elementData.ajaxUrl)) {
+                urls[elementData.ajaxUrl] = dataType;
+            }
+        });
+
+        // Now call each url and cache the response
+        $.each(urls, function(url, dataType) {
+            $.ajax(url, {
+                    dataType: dataType,
+                    success: function(response) {
+                        // Now cycle through each element using this url and populate the data
+                        $ajaxMarkupElements.each(function(index, element) {
+                            var elementData = $(element).data();
+
+                            if (elementData.ajaxUrl == url) {
+                                var innerText = $(element).text();
+
+                                if (innerText != '') {
+                                    var data = response[innerText];
+
+                                } else {
+                                    var data = response;
+                                }
+
+                                $(element).text(data); 
+                            }
+                        });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        //console.log(textStatus);
                     }
-                    else {
-                        $(element).text(response);
-                    }    				
-    			},
-    			error: function(jqXHR, textStatus, errorThrown) {
-    				//console.log(textStatus);
-    			}
-    		});
-    	});
+            });
+        });
     });
 }( jQuery ));
