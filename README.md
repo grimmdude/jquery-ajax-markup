890cd77f99755e201929048bd03e612d3e7e437e
Garrett Grimm <ggrimm@prudentpublishing.com>
2015-03-21 03:05:44 -0700

    work

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jquery-ajax-markup
 Simple jQuery library to fill a container with ajax results using HTML markup only.
+
+##Usage
+
+With this markup the contents of the div will be replaced by the response of the url.
+```html
+<div data-ajax-url="http://example.com/api/product"></div>
+```
+
+In this example `json` is specified as the data type and the `name` text will be replaced by the name property of the returned json.
+```html
+<div data-ajax-url="http://example.com/api/product" data-ajax-type="json">name</div>
+```
--- jquery-ajax-markup.js
+++ jquery-ajax-markup.js
@@ -1,11 +1,19 @@
 (function ( $ ) {
     $(function() {
     	$('*[data-ajax-url]').each(function(index, element) {
-    		//console.log($(element).data());
-    		$.ajax($(element).data('ajaxUrl'), {
-    			cache: false,
-    			success: function(data) {
-    				$(element).text(data);
+    		var innerText = $(element).text();
+    		var elementData = $(element).data();
+    		var dataType = elementData.ajaxType || false;
+            
+            $.ajax(elementData.ajaxUrl, {
+    			dataType: dataType,
+    			success: function(response) {
+                    if (innerText != '') {
+                        $(element).text(response[innerText]);
+                    }
+                    else {
+                        $(element).text(response);
+                    }    				
     			},
     			error: function(jqXHR, textStatus, errorThrown) {
     				//console.log(textStatus);
